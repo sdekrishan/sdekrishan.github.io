900fbe34912f1e69e2ae56ad1a11b3db0f622757
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-01-24 10:36:05 +0530

    new project added

--- src/Container/Work/Work.jsx
+++ src/Container/Work/Work.jsx
@@ -38,22 +38,22 @@ const Work = () => {
     setFilterWork(data)
   })},[]);
   //for github calender and stats
-  const selectLastHalfYear = contributions => {
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const shownMonths = 6;
+  // const selectLastHalfYear = contributions => {
+  //   const currentYear = new Date().getFullYear();
+  //   const currentMonth = new Date().getMonth();
+  //   const shownMonths = 6;
   
-    return contributions.filter(day => {
-      const date = new Date(day.date);
-      const monthOfDay = date.getMonth();
+  //   return contributions.filter(day => {
+  //     const date = new Date(day.date);
+  //     const monthOfDay = date.getMonth();
   
-      return (
-        date.getFullYear() === currentYear &&
-        monthOfDay > currentMonth - shownMonths &&
-        monthOfDay <= currentMonth
-      );
-    });
-  };
+  //     return (
+  //       date.getFullYear() === currentYear &&
+  //       monthOfDay > currentMonth - shownMonths &&
+  //       monthOfDay <= currentMonth
+  //     );
+  //   });
+  // };
   return (
     <>
     <h2 className='head-text'>Projects <span>Section</span></h2>
