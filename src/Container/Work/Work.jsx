9e03b6451b66293ac8c657e2f6801b67c4833f30
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-02-22 21:16:43 +0530

    hover link removed

--- src/Container/Work/Work.jsx
+++ src/Container/Work/Work.jsx
@@ -106,29 +106,6 @@ const Work = () => {
         <div className='app__work-item app__flex' key = {index}>
           <div className='app__work-img app__flex' >
         <img src= {work.imgUrl} alt ={work.name}/>
-        <motion.div
-        whileHover={{opacity:[0,1]}}
-        transition={{duration:0.25,ease:'easeInOut',staggerChildren:0.5}}
-        className="app__work-hover app__flex">
-          <a href={work.projectLink} target="_blank" rel='noreferrer'>
-            <motion.div
-            whileInView={{scale:[0,1]}}
-            whileHover={{scale:[1,0.9]}}
-            transition={{duration:0.25}}
-            className="app__flex">
-               <AiFillEye/>
-            </motion.div>
-          </a>
-          <a href={work.codeLink} target="_blank" rel='noreferrer'>
-            <motion.div
-            whileInView={{scale:[0,1]}}
-            whileHover={{scale:[1,0.9]}}
-            transition={{duration:0.25}}
-            className="app__flex">
-               <AiFillGithub/>
-            </motion.div>
-          </a>
-        </motion.div>
           </div>
           <div className='app__work-content app__flex'>
             <h4 className='work__title-text' >
