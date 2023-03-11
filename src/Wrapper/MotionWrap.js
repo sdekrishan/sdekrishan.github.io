43b382d767d40badf1efaba46fb08854e3813055
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-03-11 09:35:27 +0530

    favicon change

--- src/Wrapper/MotionWrap.js
+++ src/Wrapper/MotionWrap.js
@@ -5,8 +5,6 @@ const MotionWrap = (Component, classNames) =>
   function HOC() {
     return (
       <motion.div
-        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-        transition={{ duration: 0.5 }}
         className={`${classNames} app__flex`}
       >
         <Component />
